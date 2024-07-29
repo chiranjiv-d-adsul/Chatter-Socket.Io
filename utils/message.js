dcf0882ddfee1c95fd3f32f57328c9c69b78b877
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-07-30 01:05:45 +0530

    update

--- utils/message.js
+++ utils/message.js
@@ -1,10 +1,15 @@
-const moment = require('moment');
+function formatMessage(username, text) {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    const formattedTime = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
 
-function formatMessage(username, text){
-    return{
-        username,text,
-        time: moment().format('h:mm a')
-    }
+    return {
+        username,
+        text,
+        time: formattedTime
+    };
 }
 
 module.exports = formatMessage;
